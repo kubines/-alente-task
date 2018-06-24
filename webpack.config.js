972f98a995a51d9d20a4a6e8c95f40e90335fae8
kuba <cubax@mail.ru>
2018-06-24 20:58:38 +0700

    create project

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer'); 
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      minimize: {
+                        safe: true
+                      }
+                    }
+                  },
+                  {
+                    loader: 'postcss-loader',
+                    options: {
+                      autoprefixer: {
+                        browsers: ['last 2 versions']
+                      },
+                      plugins: () => [
+                        autoprefixer
+                      ]
+                    }
+                  },
+                  {
+                    loader: 'sass-loader'
+                  }
+                ]
+              },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'img/[name][hash].[ext]'
+                    }
+                }, {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        mozjpeg: {
+                            progressive: true,
+                            quality: 70
+                        }
+                    }
+                },
+                ],
+            }   
+        ]
+},
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        })
+    ]
+};
